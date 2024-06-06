18564f3ed29a3934c793f0d11e72bad9bffde4bb
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-06-06 18:46:05 +0300

    add

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https:// 666158c463e6a0189fe95dfc.mockapi.io/";
+axios.defaults.baseURL = "https://666158c463e6a0189fe95dfc.mockapi.io/";
 
 export const fetchTasks = createAsyncThunk(
   "contacts/fetchAll",
