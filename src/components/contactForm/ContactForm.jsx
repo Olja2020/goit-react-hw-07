f5623f615a4207ddbb78487fedbcc4db27581293
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-06-08 23:05:22 +0300

    add

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -4,9 +4,9 @@ import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { addContact } from "../../redux/contactsOps";
 import { useDispatch } from "react-redux";
-//import { isLoading } from "../App";
-import { selectLoading } from "../../redux/contactsSlice";
-const isLoading = selectLoading;
+
+
+
 const UserSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, "min 3 letter")
@@ -55,7 +55,7 @@ export default function ContactForm() {
           />
           <ErrorMessage name="number" component="span" className={css.error} />
         </div>
-        <button className={css.btnForm} type="submit" disabled={isLoading}>
+        <button className={css.btnForm} type="submit">
           Add contact
         </button>
       </Form>
