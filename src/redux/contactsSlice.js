e1264da870c0aba13181508df7177f7bc5caea37
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-06-09 15:18:16 +0300

    add

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,12 +2,7 @@ import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
 import { selectNameFilter } from "./filtersSlice";
 const initialState = {
-  items: [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ],
+  items: [],
   loading: false,
   error: false,
 };
